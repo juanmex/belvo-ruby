d7cae96995329e0876cf42da897bb9dc7197db47
Juan Mex <juanmex.me@gmail.com>
2022-07-25 18:53:11 -0500

    rubocop

--- lib/belvo/utils.rb
+++ lib/belvo/utils.rb
@@ -2,12 +2,12 @@
 
 require 'base64'
 
-# Class with helper functions
 module Belvo
+  # Class with helper functions
   class Utils
     def self.read_file_to_b64(path)
       return if path.nil? || !File.file?(path)
-  
+
       data = File.open(path).read
       Base64.encode64(data)
     end
