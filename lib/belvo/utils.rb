25b31c846fda1eb1011b2070564f2501f1ecaba5
Juan Mex <juanmex.me@gmail.com>
2022-07-25 18:49:51 -0500

    Better namespacing is to add the namespace to Utils class

--- lib/belvo/utils.rb
+++ lib/belvo/utils.rb
@@ -3,12 +3,14 @@
 require 'base64'
 
 # Class with helper functions
-class Utils
-  def self.read_file_to_b64(path)
-    return if path.nil? || !File.file?(path)
-
-    data = File.open(path).read
-    Base64.encode64(data)
+module Belvo
+  class Utils
+    def self.read_file_to_b64(path)
+      return if path.nil? || !File.file?(path)
+  
+      data = File.open(path).read
+      Base64.encode64(data)
+    end
   end
 end
 
