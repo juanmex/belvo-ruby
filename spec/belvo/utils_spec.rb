3f2d647e7cf0c47ee5c0d21ada022137b8599bbe
Juan Mex <juanmex.me@gmail.com>
2022-07-26 14:55:16 -0500

    Fix broken spec

--- spec/belvo/utils_spec.rb
+++ spec/belvo/utils_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe Utils do
+RSpec.describe Belvo::Utils do
   it 'read_file_to_b64 returns nil with bad file' do
     expect(described_class.read_file_to_b64('/bad/path')).to eq(nil)
   end
